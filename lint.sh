43c1f081881acb28109724d558e1a7d63ad125b2
Yuan Li <yuanli@192-168-1-109.tpgi.com.au>
2021-05-14 23:26:41 +1000

    init

--- lint.sh
+++ lint.sh
@@ -6,37 +6,43 @@
 newnumber=$(curl --silent "https://api.github.com/repos/aws-cloudformation/cfn-python-lint/releases/latest" | jq -r .tag_name | cut -d'v' -f 2)
 echo "The latest release tag is "$newnumber
 
-
-# Get current number
-#currentvalue=$(<num.txt)
-#echo "The current release tag is "$currentvalue
+# num.txt will keep the previous tag number
 FILE=num.txt
 if [ -f "$FILE" ];
 then
   oldnumber=$(<num.txt)
-  echo "The current record is " $oldnumber
+  echo "The current tag is " $oldnumber
 
+#if the record didn't match then update the contents and pull new data , build and push to docker hub
 
   if [ $newnumber != $oldnumber ];
   then
       echo $newnumber > num.txt
-      echo "Updated the num.txt record "
-      git clone https://github.com/aws-cloudformation/cfn-python-lint.git
-      echo "git clone completed"
+      echo "********Updated the num.txt record*****"
       cd cfn-python-lint
-      echo "Built image"
-      docker build beanxyz/lint:latest .
-      echo "Push to Docker hub"
-      docker tag beanxyz/lint:$newnumber
+      git pull
+      echo "*******Built image*********"
+      docker build -t beanxyz/lint:latest .
+      echo "*******Push to Docker hub********"
+      docker tag beanxyz/lint:latest beanxyz/lint:$newnumber
       docker push beanxyz/lint:latest
       docker push beanxyz/lint:$newnumber
 
   else
-      echo "No update"
+      echo "****** No update ********"
+
   fi
 
 else
-      echo "num.txt didn't exist, let's create a new one"
-      echo $newnumber > "./num.txt"
+      echo "*****num.txt didn't exist, let's create a new one*******"
+      echo $newnumber > num.txt
+      git clone https://github.com/aws-cloudformation/cfn-python-lint.git
+      cd cfn-python-lint
+      echo "*****Built image*******"
+      docker build -t beanxyz/lint:latest .
+      echo "*******Push to Docker hub*****"
+      docker tag beanxyz/lint:latest beanxyz/lint:$newnumber
+      docker push beanxyz/lint:latest
+      docker push beanxyz/lint:$newnumber
 
 fi
